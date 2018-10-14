64042c791040b579aee5cb3fb55a15f13e97a091
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2018-10-13 22:36:05 -0400

    Created a Queue class

---
+++ collections/queue/queue.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    enqueue(val) {
+        let n = new Node(val);
+        if (!this.head) {
+            this.head = n;
+            this.tail = n;
+        }
+        else {
+            this.tail.next = n;
+            this.tail = n;
+        }
+        return ++this.size;
+    }
+    dequeue() {
+        if (!this.head) return null;
+        let popped = this.head;
+        let poppedVal = popped.val;
+        this.head = this.head.next;
+        if (!this.head) {
+            this.tail = null;
+        }
+        popped.next = null;
+        this.size--
+        return poppedVal;
+    }
+}
+
+module.exports = Queue;
